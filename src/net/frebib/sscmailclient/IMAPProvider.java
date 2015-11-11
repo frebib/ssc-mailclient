ce536b5afb74869edf9448bb8e670927134ea74d
Joseph Groocock <frebib@gmail.com>
2015-11-11 00:10:45 +0000

    Add IMAP mail provider & interface

---
+++ src/net/frebib/sscmailclient/IMAPProvider.java
@@ -0,0 +1,55 @@
+package net.frebib.sscmailclient;
+
+import javax.mail.*;
+import java.util.Properties;
+
+public class IMAPProvider implements MailProvider {
+    private Properties props;
+    private Session session;
+    private Store store;
+
+    public IMAPProvider(Properties props) throws IllegalArgumentException {
+        if (props == null)
+            throw new IllegalArgumentException("Properties cannot be null");
+
+        props.setProperty("mail.store.protocol", "imaps");
+        this.props = props;
+        this.session = Session.getInstance(props);
+        try {
+            this.store = session.getStore("imaps");
+        } catch (NoSuchProviderException e) {
+            MailClient.log.exception(e);
+        }
+    }
+
+    @Override
+    public void Connect() throws MessagingException {
+        store.connect(props.getProperty("mail.host"),
+                      props.getProperty("mail.user"),
+                      props.getProperty("mail.password")
+        );
+    }
+
+    @Override
+    public Session getSession() {
+        return session;
+    }
+
+    @Override
+    public Folder getFolder(String path) throws MessagingException {
+        return store.getFolder(path);
+    }
+
+    @Override
+    public Message[] getMessages(String path) throws MessagingException {
+        Folder folder = getFolder(path);
+        if(!folder.isOpen())
+            folder.open(Folder.READ_WRITE);
+        return folder.getMessages();
+    }
+
+    @Override
+    public Folder[] getFolderList() throws MessagingException {
+        return store.getDefaultFolder().list();
+    }
+}
---
+++ src/net/frebib/sscmailclient/MailProvider.java
@@ -0,0 +1,15 @@
+package net.frebib.sscmailclient;
+
+import javax.mail.Folder;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import java.util.Properties;
+
+public interface MailProvider {
+    Session getSession();
+    void Connect() throws MessagingException;
+    Folder[] getFolderList() throws MessagingException;
+    Folder getFolder(String path) throws MessagingException;
+    Message[] getMessages(String path) throws MessagingException;
+}
